bc162cc1fb6198c5de93b392512aa3bee5349267
luluwaha <lululoka88@gmail.com>
2019-08-20 23:33:15 +0300

    100DaysOfCode

---
+++ 1st Lesson.py
@@ -0,0 +1,9 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>> if 5 > 2:
+	print("Five is grater than two!")
+
+Five is grater than two!
+>>> 
