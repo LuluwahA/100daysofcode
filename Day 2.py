ac50017f355cb659a624e24541a9edcf2fbf0e11
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-08-21 21:35:01 +0300

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,24 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #this is a comment
+>>> print("Hello, World!")
+Hello, World!
+>>> print("Hello, World!")#this is a comment
+Hello, World!
+>>> #print("Hello, World!")
+>>> print("Cheers, Mate!")
+Cheers, Mate!
+>>> #this is  cooment
+>>> #written in
+>>> #more than just one line
+>>> print("Hello, World!")
+Hello, World!
+>>> """
+this is a comment
+written in
+more than one line
+"""
+'\nthis is a comment\nwritten in\nmore than one line\n'
+>>> print("Hello, World!")
+Hello, World!
+>>> 
