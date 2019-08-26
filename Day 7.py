4b2864b25b35c405cb54a1c6faa218353d3f83a8
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-08-26 23:11:30 +0300

    Add files via upload

---
+++ Day 7.py
@@ -0,0 +1,17 @@
+print("hello")
+print('hello')
+
+x="hello"
+print(x)
+
+a= """hi there
+this in not
+my first python code
+bye now, we will meet again. """
+print(a)
+
+x="hello, world!"
+print(x[1])
+
+b= "hello there"
+print(b[2:5])
