5ee51519fdae6dbd424adeea2cdaed93d08ecf63
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-08-27 00:52:39 +0300

    Add files via upload

---
+++ Day 8.py
@@ -0,0 +1,25 @@
+#strip()
+
+x=" Hello, My Friend "
+print(x.strip())
+
+#len()
+
+x="Hello, My Friend"
+print(len(x))
+
+#lower()
+x="Hello, My Friend"
+print(x.lower())
+
+#upper
+x="Hello, My Friend"
+print(x.upper())
+
+#replace
+x="Hello, My Friend"
+print(x.replace("H","M"))
+
+#slpit
+x="Hello, My Friend"
+print(x.split(","))
