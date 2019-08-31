7e658b7134cdf83a403693db09b73bcfbec82877
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-01 01:43:49 +0300

    Add files via upload

---
+++ Day 11.py
@@ -0,0 +1,17 @@
+x= 6
+print(x>3 or x<5)
+
+
+x=['apple','orange']
+y=['apple','orange']
+z=x
+print(x is not z)
+print(x is not y)
+print(x is y)
+print(x!=y)
+
+x=['apple','orange']
+print('orange' in x)
+print('banana' not in x)
+
+
