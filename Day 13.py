177d6e52570f8ee74db95e0daf330fa516e7e266
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-02 02:53:36 +0300

    Add files via upload

---
+++ Day 13.py
@@ -0,0 +1,37 @@
+list=[]
+print(list)
+
+num=[1,2,3,4,5]
+print(num)
+
+a=['january', 'march','may']
+print(a)
+
+c=['january', 'march','may',1,2,3]
+print(c)
+
+z=[12.3, 34.2, 99.5]
+print(z)
+
+a=['january', 'march','may']
+print(a[1])
+
+list=['january', 'march','may']
+for x in list:
+ print(x)
+
+num=[1,2,3,4,5]
+for y in num:
+ print(y)
+
+a=['january','march','may']
+a[1]='september'
+print(a)
+
+a=['january', 'march','may']
+del a[2]
+print(a)
+
+a=['january', 'march','may']
+del a
+print(a)
---
+++ Day 14.py
@@ -0,0 +1,24 @@
+list=['march', 'september','april','october']
+print(list[1:3])
+
+list=[1,2,3,4,5,6]
+print(list[2:5])
+
+
+list=['march', 'september','april','october']
+if 'april' in list:
+ print("yes 'april' is in the list")
+
+a=['Im fabluse']*3
+print(a)
+
+list1=['march', 'september','april','october']
+list2=[3,9,4,10]
+x=list1+list2
+print(x)
+
+
+list1=[33.2,5.3,99.1]
+list2=[3,9,4,10]
+x=list1+list2
+print(x)
