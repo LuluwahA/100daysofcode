fa15c2e80923d60ea7127988bb121eb9b685ea7e
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-07 18:55:10 +0300

    Add files via upload

---
+++ Day 18&19.py
@@ -0,0 +1,24 @@
+#____First Task_____
+mylist=[1,2,3,"one","two"]
+
+
+mylist.insert(3,4)
+print(mylist)
+
+mylist.append("three")
+print(mylist)
+
+mylist.copy()
+print(mylist)
+
+mylist.reverse()
+print(mylist)
+
+#____Second Task____
+
+mytuple=("java","python","swift")
+if "python" in mytuple:
+    print("YES, python is in mytuple")
+else:
+    print("NO, python is not in mytuple")
+
