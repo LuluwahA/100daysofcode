350d9c7a8e3dcf6ed5b6a1d65688ba5d451a2ddc
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-10 03:24:32 +0300

    Add files via upload

---
+++ Day 22.py
@@ -0,0 +1,80 @@
+thisdict={}
+print(thisdict)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+print(thisdict)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+x=thisdict["may"]
+print(x)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+x=thisdict.get("april")
+print(x)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+thisdict["january"]=1
+print(thisdict)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+for x in thisdict:
+  print(x)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+for x in thisdict:
+  print(thisdict[x])
+
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+for x in thisdict.values():
+  print(x)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+print(thisdict.values())
+
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+for x,y in thisdict.items():
+  print(x,y)
+
+thisdict={
+    "january":"first",
+    "may":"fifth",
+    "april":4}
+print(thisdict.items())
+
+
+
+
+
+
+
+
+  
