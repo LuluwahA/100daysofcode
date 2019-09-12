676d4f2ea5e398262631903346714e95722aa55a
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-13 01:10:38 +0300

    Add files via upload

---
+++ Day 25&26.py
@@ -0,0 +1,18 @@
+#First_Task
+myset={1,3,5,7,8}
+myset.update([4,8,12])
+print(myset)
+
+myset.clear()
+print(myset)
+
+
+#Second_Task
+mydiction={
+    "name":"pigeon",
+    "type":"bird",
+    "skin cover":"feathers"}
+x=mydiction.get("type")
+print(x)
+mydiction["skin cover"]= "featherless"
+print(mydiction)
