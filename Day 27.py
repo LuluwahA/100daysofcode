509b3764d68fe57471d6412ac47bfcb132d803ba
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-16 03:07:16 +0300

    Add files via upload

---
+++ Day 27.py
@@ -0,0 +1,62 @@
+x=54
+y=21
+if x>y:
+    print("x is bigger than y")
+
+x=21
+y=21
+if x>y:
+    print("x is bigger than y")
+elif x==y:
+    print("x and y are equals") 
+
+x=21
+y=210
+if x>y:
+    print("x is bigger than y")
+elif x==y:
+    print("x and y are equals")
+else:
+    print("y is bigger than x")
+
+   
+x=5
+y=21
+if x>y:
+    print("x is bigger than y")
+else:
+    print("y is bigger than x")
+
+
+x=54
+y=21
+if x>y: print("x is bigger than y") 
+       
+x=54
+y=21
+print("y")if y>x else print("x")
+
+x=54
+y=54
+print("x")if x>y else print("=") if x==y else print("y")
+
+x=54
+y=21
+z=87
+if x>y and z>y:
+    print("both conditions are true")
+    
+x=54
+y=21
+z=87
+if x>y or x>z:
+    print("both conditions are true")
+
+x=43
+if x>10:
+    print("Above ten,")
+    if x>20:
+        print("and also above 20")
+    else:
+        print("but not above 20")
+
