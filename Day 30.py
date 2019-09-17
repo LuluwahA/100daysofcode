8c1813f7fcebb6717a0880b413c48f32f96f3c46
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-18 02:25:40 +0300

    Add files via upload

---
+++ Day 30.py
@@ -0,0 +1,21 @@
+for x in range(6):
+    print(x)
+
+for x in range(2,7):
+    print(x)
+
+for x in range(2,30,3):
+    print(x)
+
+    
+for x in range(6):
+    print(x)
+else:
+    print("finished")
+
+d=["red","big","tasty"]
+f=["apple","banana","cherry"]
+for x in d:
+    for y in f:
+      print(x,y)
+      
