20921f0ff5cf938c44c6fff3892ba8c78f66a505
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-19 01:12:21 +0300

    Add files via upload

---
+++ Day 31.py
@@ -0,0 +1,17 @@
+def myfun():
+    print("hello there")
+myfun()    
+
+def myfun(fname):
+    print(fname+" jackson")
+myfun("Email")
+myfun("Sam")
+myfun("Lura")
+
+def myfun(count ="Norway"):
+    print("Im from "+count)
+myfun("Sweden")
+myfun("India")
+myfun()
+myfun("Brazil")
+
