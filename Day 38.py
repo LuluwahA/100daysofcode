deb266128d7e63d3d30c445041e0a35d39e891a6
Luluwah A <54288653+LuluwahA@users.noreply.github.com>
2019-09-26 00:01:21 +0300

    Add files via upload

---
+++ Day 38.py
@@ -0,0 +1,15 @@
+car =["ford","volvo","BMW"]
+for x in car:
+    print(x)
+    
+car =["ford","volvo","BMW"]
+car.append("honda")
+print(car)
+
+car =["ford","volvo","BMW"]
+car.pop(0)
+print(car)
+
+car =["ford","volvo","BMW"]
+car.remove("volvo")
+print(car)
